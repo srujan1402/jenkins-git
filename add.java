0771e1bb04a9b4f8e9b02d611a384e0d61a769bc
Srujan reddy <mallesh.srujan.reddy@gmail.com>
2026-01-08 15:29:44 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,6 @@
+public class add {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins!");
+        System.out.println("Java program executed successfully.");
+    }
+}
